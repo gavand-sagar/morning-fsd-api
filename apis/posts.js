69b344713d2514a1fa120e30c335f68229e0c567
gavand-sagar <gavand.sagar.s@gmail.com>
2022-11-13 09:47:17 +0530

    likes with new model

--- apis/posts.js
+++ apis/posts.js
@@ -35,7 +35,6 @@ postRoutes.post("/posts", async (req, res) => {
   let obj = req.body;
 
   obj["likes"] = 0;
-  obj["likedUsers"] = [];
   obj["commentsCount"] = 0;
 
   await saveItemInCollection("posts", obj);
@@ -60,38 +59,38 @@ postRoutes.put("/posts", (req, res) => {
 });
 
 //likes
-postRoutes.patch("/posts/:id/like", async (req, res) => {
-  let id = req.params.id;
-  let username = req.body.username;
-  //get item from database
-  let post = await getSingleItemFromCollection("posts", { _id: ObjectId(id) });
+// postRoutes.patch("/posts/:id/like", async (req, res) => {
+//   let id = req.params.id;
+//   let username = req.body.username;
+//   //get item from database
+//   let post = await getSingleItemFromCollection("posts", { _id: ObjectId(id) });
 
-  //checking if found
-  if (post) {
-    if (!post.likedUsers.some((x) => x == username)) {
-      //update likes on server
-      post.likedUsers.push(username);
-      post.likes = post.likedUsers.length;
+//   //checking if found
+//   if (post) {
+//     if (!post.likedUsers.some((x) => x == username)) {
+//       //update likes on server
+//       post.likedUsers.push(username);
+//       post.likes = post.likedUsers.length;
 
-      //update likes on database
-      await updateItemFromCollection("posts", { _id: ObjectId(id) }, post);
-      //done
-      return res.json({
-        result: true,
-        newLikes: post.likes,
-      });
-    } else {
-      return res.json({
-        result: true,
-        newLikes: post.likes,
-      });
-    }
-  }
+//       //update likes on database
+//       await updateItemFromCollection("posts", { _id: ObjectId(id) }, post);
+//       //done
+//       return res.json({
+//         result: true,
+//         newLikes: post.likes,
+//       });
+//     } else {
+//       return res.json({
+//         result: true,
+//         newLikes: post.likes,
+//       });
+//     }
+//   }
 
-  return res.json({
-    result: false,
-  });
-});
+//   return res.json({
+//     result: false,
+//   });
+// });
 
 //getting all the comments for the post
 postRoutes.get("/posts/:id/comments", async (req, res) => {
@@ -128,5 +127,41 @@ postRoutes.post("/posts/:id/comments", async (req, res) => {
     newComments: newComments,
   });
 });
+
+
+
+postRoutes.get("/posts/:id/likes", async (req, res) => {
+  let likes = await getAllItemsFromCollection("likes", {
+    postId: req.params.id,
+  });
+
+  return res.json({
+    result: true,
+    likes: likes,
+  });
+});
+
+postRoutes.post("/posts/:id/likes", async (req, res) => {
+  let obj = req.body;
+  obj["postId"] = req.params.id;
+
+  await saveItemInCollection("likes", obj);
+
+  let likes = await getAllItemsFromCollection("likes", {
+    postId: req.params.id,
+  });
+
+  //Update the comment count in post object?
+  await updateItemFromCollection(
+    "posts",
+    { _id: ObjectId(req.params.id) },
+    { likes: likes.length }
+  );
+
+  return res.json({
+    result: true,
+    likes: likes,
+  });
+})
 
 export default postRoutes;
